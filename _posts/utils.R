d4f799fd376f12e80232dbe7cb85f3a9a8b8afd4
Bryan Whiting <bryan.g.whiting@gmail.com>
2021-09-01 12:10:38 -0700

    Adding all posts

--- _posts/utils.R
+++ _posts/utils.R
@@ -11,11 +11,28 @@
 #' @export
 #'
 #' @examples
-quote_paragraph <- function(df, phrase = "xxxx"){
+quote_paragraph <- function(df, phrase = "xxxx") {
   # assert n rows is 1
   date <- df$date %>% format("%b %Y")
-  text <- str_replace(df$paragraph, phrase, glue("**{phrase}**"))
+  text <- str_replace(
+    df$paragraph, phrase,
+    glue('<strong style="color:{colors[1]}">{phrase}</strong>')
+  )
   title <- glue("[{df$title1}, {date}]({df$deep_link})")
   msg <- glue("\n\n>{text}\n\n\n>{df$author1} _{title}_\n\n")
+  cat(msg)
+}
+
+quote_verse <- function(df, phrase = "xxxx") {
+
+  # TODO: add deep link
+  # assert n rows is 1
+  text <- str_replace(
+    df$text[1],
+    phrase,
+    glue('<strong style="color:{colors[1]}">{phrase}</strong>')
+  )
+  source <- glue("{df$volume_short_title} {df$chapter_number}:{df$verse_number}")
+  msg <- glue("\n\n> {text}({source}) \n\n")
   cat(msg)
 }
