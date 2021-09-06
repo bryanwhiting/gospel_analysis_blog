af29cf0abecb3fbcc3b625ba2cd1e8b46443a947
Bryan Whiting <bryan.g.whiting@gmail.com>
2021-09-06 15:48:09 -0700

    Moved utils, ggtheme and test_airtabler

--- R/test_airtabler.R
+++ R/test_airtabler.R
@@ -1,26 +1,22 @@
 # Test airtabler
 library(airtabler)
+library(lubridate)
+library(glue)
 
 SocialMediaPosts <-
   airtable(
-    base = "app4FiCy5NRkbihg8",
+    base = "applRcAPoTh7RI9O4",
     tables = c("Scheduled")
   )
 scheduled <- SocialMediaPosts$Scheduled$select()
 
-record_data <- list(
-  Date = "New hotel",
-  `Price/night` = 200,
-  Stars = "****",
-  Amenities = c("Hiking", "Gym"),
-  Notes = "Just a sample record.\nWith extra line in notes."
-)
 new_post <- list(
-  date = '2021-09-04',
-  # attachments = qplot('hi'),
+  created_date = now(tz = "US/Pacific"),
   attachment_url = "https://www.gospelanalysis.com/posts/second-coming/second-coming_files/figure-html5/unnamed-chunk-3-1.png",
-  copy = "testing out some automation",
-  tagged_users = ""
+  copy = glue("test at 3:36pm"),
+  tagged_users = "@gospelanalysis",
+  status = "backlog",
+  scheduled_datetime = ymd_hms("2021-09-06 16:15:00", tz= "US/Pacific")
 )
 resp <- SocialMediaPosts$Scheduled$insert(new_post)
 
--- README.md
+++ README.md
@@ -20,3 +20,19 @@ github.com/mfherman/westchester-covid/blob/master/site/county.Rmd
 ```
 xaringanExtra::use_panelset()
 ```
+
+## Airtable, Zapier, and Instagram
+
+1. Create new Instagram account with new email address (don't connect it to a personal account)
+1. Create airtable account and new base + table
+1. Get Airtable API key from https://airtable.com/account
+1. Save to .Renviron `AIRTABLE_API_KEY=keyxxxxxxxxx`
+1. Restart R Console
+1. Get Airtable App ID (if it's not in the URL, check help > API Documentation and the new URL should have it.)
+1. Edit test_airtabler.R script with APP ID
+1. Upload example data
+1. Create Zapier
+1. Connect Zapier to Airtable using API key
+1. Build a new Zap with Airtable as trigger and Instagram Business Post Photo as action
+1. Can't have a private user in the "tagged_users" field.
+1. Zap: https://zapier.com/shared/b2bf1c15f536497329b88201d25786ab34ecdd1e
