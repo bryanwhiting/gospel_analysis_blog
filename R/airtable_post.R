dabd912dd0d993cbb4229e29ed9eb7978b061f79
Bryan Whiting <bryan.g.whiting@gmail.com>
2021-09-13 20:33:18 +0000

    refactored aritbale_post

---
+++ R/airtable_post.R
@@ -0,0 +1,85 @@
+source(here::here("R/social_media.R"))
+
+airtable_base <- "applRcAPoTh7RI9O4"
+root_img <- here::here("img")
+insta_hashtags <- "
+__________________________
+📷: www.gospelanalysis.com
+📧: gospelanalysisnow@gmail.com - ask us a question!
+__________________________
+#churchofjesuschristoflatterdaysaints #churchofjesuschrist #hearhim
+#generalconference #scripture #scriptures
+#lds #ldsquotes #mormon
+#jesuschrist #jesus #jesús #christ #savior
+#christian #christianity #christianitypost #christianityquote
+#discipleship
+#data #datascience #dataanalytics #datascientist #dataviz
+#religion #faith #faithquotes #faithoverfear
+"
+twitter_hashtags <- "#jesus #christ #faith #religion"
+stopifnot(stringr::str_count(insta_hashtags, "#") <= 30)
+
+get_img_files <- function(slug) {
+  path <- file.path(root_img, slug)
+  dirs <- list.files(path, full.names = T, recursive = F)
+  out <- list()
+  for (d in dirs) {
+    name <- basename(d)
+    p <- file.path("https://www.gospelanalysis.com/img", slug)
+    out[[name]]$plot <- file.path(p, name, "plot.png")
+    out[[name]]$title <- read_content(slug, name, "title")
+    out[[name]]$body <- read_content(slug, name, "body")
+  }
+  out
+}
+
+
+airtable_post <- function(slug, name = NULL, tab_name = "Scheduled") {
+  # specify name if you want to only upload one
+  # tab_name = "Testing"
+  posts <- get_img_files(slug)
+
+  SocialMediaPosts <-
+    airtabler::airtable(
+      base = airtable_base,
+      tables = c(tab_name)
+    )
+  # Read posts
+  # scheduled <- SocialMediaPosts[[tab_name]]$select()
+
+  if (!is.null(name)) {
+    post_names <- name
+  } else {
+    post_names <- names(posts)
+  }
+  for (post in post_names) {
+    slug_name <- glue::glue("{slug}-{post}")
+    plot_url <- posts[[post]]$plot
+    copy_title <- posts[[post]]$title
+    copy_body <- posts[[post]]$body
+
+    post_url <- fs::path("https:://www.gospelanalysis.com/posts", slug)
+
+    # Create copy
+    insta_fb_copy <- glue::glue("{copy_title}\n{copy_body}\nRead more: {post_url}\n{insta_hashtags}")
+    twitter_copy <- glue::glue("{copy_title} {post_url} {twitter_hashtags}")
+
+    new_post <- list(
+      created_date = now(tz = "US/Pacific"),
+      slug_name = slug_name,
+      post_url = post_url,
+      title = copy_title,
+      twitter_copy = twitter_copy,
+      insta_fb_copy = insta_fb_copy,
+      img_url = plot_url,
+      tagged_users = "@gospelanalysis",
+      status = "backlog"
+      # TODO: add scheduling (requires premium?)
+      # scheduled_datetime = ymd_hms("2021-09-06 16:15:00", tz= "US/Pacific")
+    )
+    resp <- SocialMediaPosts[[tab_name]]$insert(new_post)
+    msg = glue("Uploaded to airtable: {slug_name}")
+    cli::cli_alert_success(msg)
+  }
+  invisible(resp)
+}
--- tests/testthat/test-social_media.R
+++ tests/testthat/test-social_media.R
@@ -56,22 +56,6 @@ test_that("write_content() and read_content() works", {
   teardown()
 })
 
-test_that("get_img_files() works", {
-  p <- ggplot2::ggplot()
-  save_img(p, slug, "dir1")
-  write_content("hello1", slug, "dir1", "title")
-  write_content("hello2", slug, "dir1", "body")
-  save_img(p, slug, "dir2")
-  write_content("hello3", slug, "dir2", "title")
-  write_content("hello4", slug, "dir2", "body")
-  ans <- get_img_files(slug)
-  expect_equal(length(ans), 2)
-  expect_equal(ans$dir1$plot, "https://www.gospelanalysis.com/img/test/dir1/plot.png")
-  expect_equal(ans$dir2$title, "hello3")
-  expect_equal(ans$dir2$body, "hello4")
-  teardown()
-})
-
 test_that("make_post() works", {
   p <- ggplot2::ggplot()
   make_post(img = p, title = "title", body = "body", slug, name, is_quote = F)
@@ -79,15 +63,5 @@ test_that("make_post() works", {
   expect_equal(ans$test$plot, "https://www.gospelanalysis.com/img/test/test/plot.png")
   expect_equal(ans$test$title, "title")
   expect_equal(ans$test$body, "body")
-  teardown()
-})
-
-test_that("airtable_post() works", {
-  p <- ggplot2::ggplot()
-  make_post(img = p, title = "title", body = "body", slug = "test", name = "test", is_quote = F)
-  posts <- get_img_files("test")
-  ans <- airtable_post(slug, name = NULL, tab_name = "Testing")
-  expect_equal(ans$fields$img_url, "https://www.gospelanalysis.com/img/test/test/plot.png")
-  expect_true(str_detect(ans$fields$twitter_copy, fixed("#")))
   teardown()
 })
